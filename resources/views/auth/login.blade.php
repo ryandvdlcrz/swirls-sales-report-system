367fbb72422ff70fb587bfb843c8080b7a8af153
ryandvdlcrz <194396889+ryandvdlcrz@users.noreply.github.com>
2026-02-19 00:41:23 +0800

    fixed other pages to use vite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manong Sorbetero </title>
-    <link rel="stylesheet" href="{{asset('css/output.css')}}">
+    <title>Swirls Login</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
    
 
 <body >
--- resources/views/settings/get_help.blade.php
+++ resources/views/settings/get_help.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> tulong ni Manong Sorbetero</title>
 
-    <link rel="stylesheet" href="{{asset('css/output.css')}}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 <body>
